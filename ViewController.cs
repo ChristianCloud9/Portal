563759199a78ca0571d5c17cc8fb0160077434ef
Christian <86079954+ChristianCloud9@users.noreply.github.com>
2021-12-14 13:55:09 +0000

    Add files via upload

---
+++ ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace Portal
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        partial void SubmitBtn(NSButton sender)
+        {
+            var name = NameOutput.StringValue;
+            var email = EmailOutput.StringValue;
+            var checkbox = RememberMe.State;
+
+            string data = " \nValue of Name field: " + name + " \nValue of Email field: " + email + " \nValue of Remember me field: " + checkbox;
+
+            Console.WriteLine(data);
+        }
+    }
+}
